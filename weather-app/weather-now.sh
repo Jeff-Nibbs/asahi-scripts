77729680ef0ec0416536f0eedab8dabbbbbb4a1b
Mr. Mans <98377720+Jeff-Nibbs@users.noreply.github.com>
2025-07-04 17:59:21 -0700

    added max temp

--- weather-app/weather-now.sh
+++ weather-app/weather-now.sh
@@ -31,17 +31,23 @@ quote=$( curl -s "https://api.adviceslip.com/advice" | jq ".slip.advice" )
 
 kanye_quote=$( curl -s "https://api.kanye.rest" | jq ".quote" )
 
-curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$my_lat&lon=$my_lon&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_my_weather.txt
+# curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$my_lat&lon=$my_lon&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_my_weather.txt
 
-curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$store_lat&lon=$store_lon&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_store_weather.txt
+curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$store_lat&lon=$store_lon&exclude=current,minutely,hourly,alerts&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_store_weather.txt
+
+curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$my_lat&lon=$my_lon&exclude=current,minutely,hourly,alerts&appid=$WEATHER_KEY&units=imperial" > $folder_path/cahe_my_weather.txt
 
 my_current_temp=$( cat $folder_path/cahe_my_weather.txt | jq ".main.temp" )
+
+my_current_max_temp=$( cat $folder_path/cahe_my_weather.txt | jq ".main.temp_max" )
 
 my_current_desc=$( cat $folder_path/cahe_my_weather.txt | jq ".weather.[0].description" | tr -d '"' )
 
 my_current_desc=$( cat $folder_path/cahe_my_weather.txt | jq ".weather.[0].description" | tr -d '"' )
 
 store_current_temp=$( cat $folder_path/cahe_store_weather.txt | jq ".main.temp" )
+
+store_current_max_temp=$( cat $folder_path/cahe_store_weather.txt | jq ".main.temp_max" )
 
 store_current_desc=$( cat $folder_path/cahe_store_weather.txt | jq ".weather.[0].description" | tr -d '"' )
 
@@ -66,11 +72,13 @@ echo " "
 center "Home"
 center "Temp: ${my_current_temp}${fahrenheit}"
 center "Feels Like: ${my_feels_like}${fahrenheit}"
+center "Max Temp: ${my_current_max_temp}${fahrenheit}"
 center  "$my_current_desc outside"
 echo " "
 center "Work"
 center "Temp: ${store_current_temp}${fahrenheit}"
 center "Feels Like: ${store_feels_like}${fahrenheit}"
+center "Max Temp: ${store_current_max_temp}${fahrenheit}"
 center "$store_current_desc outside"
 echo " "
 center "Sunrise: $( format_time $sunrise )"
